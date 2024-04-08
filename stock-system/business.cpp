382a38ddc40dc9ecf6424f8df698e0796f9496b8
junyan <junyantang1008@gmail.com>
2024-04-08 13:49:19 -0400

    fix logical bug

--- stock-system/business.cpp
+++ stock-system/business.cpp
@@ -53,7 +53,7 @@ ResultC Creation::createStock(string sym, string account_id, double amount) {
             W.exec("INSERT INTO SHARE (SHAREID, ACCOUNT_ID, NUM) VALUES (" + W.quote(sym) + ", " + W.quote(account_id) + ", " + W.quote(amount) + ")");
         } else {
      
-            double current_amount = R.begin()[2].as<double>(); 
+            double current_amount = R.begin()[3].as<double>(); 
             double total = current_amount + amount;
             W.exec("UPDATE SHARE SET NUM = " + W.quote(total) + " WHERE SHAREID = " + W.quote(sym) + " AND ACCOUNT_ID = " + W.quote(account_id));
         }
@@ -82,46 +82,18 @@ ResultT Transact::openOrder(string account_id, string sym, string amount, string
     trans_history.push_back(curr);
 
     try {
-        work W(*C); // 开始一个新事务
-
+    
         if (shares < 0) { // 处理卖单
-            result R = W.exec("SELECT * FROM SHARE WHERE SHAREID = " + W.quote(sym) + " AND ACCOUNT_ID = " + W.quote(account_id) + " FOR UPDATE");
-            double curr_shares = R.size() == 0 ? 0 : R.begin()[2].as<double>();
-
-            if (curr_shares >= abs(shares)) {
-                db.insert_sell_order(sym, account_id, abs(shares), price, timestamp, trans_id);
-                db.update_stock(sym, account_id, curr_shares + shares); // 这里应该是减去卖出的股票数量
-                db.insert_transaction(trans_id, timestamp, account_id, sym, shares, price, "open");
-                res = {account_id, "order", trans_id, sym, "success", "", trans_history};
-            } else {
-                W.abort(); // 没有足够的股票，取消事务
-                res = {account_id, "order", trans_id, sym, "error", "Not enough shares", trans_history};
-                return res;
-            }
+            res = db.open_sell_order(account_id, sym, shares, price, trans_id);
         } else if (shares > 0) { // 处理买单
-            result R = W.exec("SELECT * FROM ACCOUNT WHERE ACCOUNT_ID = " + W.quote(account_id) + " FOR UPDATE");
-            double balance = R.begin()[1].as<double>();
-            double money_needed = shares * price;
-
-            if (balance >= money_needed) {
-                db.insert_buy_order(sym, account_id, shares, price, timestamp, trans_id);
-                db.update_account(account_id, balance - money_needed);
-                db.insert_transaction(trans_id, timestamp, account_id, sym, shares, price, "open");
-                res = {account_id, "order", trans_id, sym, "success", "", trans_history};
-            } else {
-                W.abort(); // 余额不足，取消事务
-                res = {account_id, "order", trans_id, sym, "error", "Not enough balance", trans_history};
-                return res;
-            }
+            res = db.open_buy_order(account_id, sym, shares, price, trans_id);
         }
-
-        W.commit(); // 提交事务
     } catch (const std::exception& e) {
         cerr << "Transaction failed: " << e.what() << endl;
         // 这里可能需要根据异常类型决定是否回滚事务
         res = {account_id, "order", trans_id, sym, "error", "Transaction failed", trans_history};
     }
-
+    res.transaction = trans_history;
     mkt.match_sell(); // 这个方法需要在事务外执行，并且也需要处理并发问题
     return res;
 }
@@ -205,17 +177,3 @@ ResultT Transact::queryOrder(string trans_id) {
     return res;
 }
 
-
-bool Transact::checkAccount(string account_id) {
-    try {
-
-        work W(*C);
-        result R = W.exec("SELECT ACCOUNT_ID FROM ACCOUNT WHERE ACCOUNT_ID = " + W.quote(account_id));
-        W.commit();
-        return R.size() != 0;
-    } catch (const std::exception &e) {
-        cerr << "Exception in checkAccount: " << e.what() << endl;
-   
-        return false;
-    }
-}
